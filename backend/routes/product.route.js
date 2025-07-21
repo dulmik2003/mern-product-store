21090b0b564808801f9164c8f3567dc2ee52043f
Dulmika <semaldulmika@gmail.com>
2025-07-21 14:23:51 +0530

    Implement a delete API for remove products.

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createProduct, getProducts, updateProduct } from "../controllers/product.controller.js";
+import { createProduct, deleteProduct, getProducts, updateProduct } from "../controllers/product.controller.js";
 
 const router = express.Router();
 
 router.post("/", createProduct);
 router.get("/", getProducts);
 router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct)
 
 export default router;
