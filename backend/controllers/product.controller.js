358912395de45a231ad6c4295cf69705e669bf48
Dulmika <semaldulmika@gmail.com>
2025-07-21 14:20:23 +0530

    Implement a put API for update each product.

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Product from "../model/product.model.js";
 
 export const createProduct = async (req, res) => {
@@ -23,6 +24,22 @@ export const getProducts = async (req, res) => {
 		res.status(200).json({ success: true, data: products });
 	} catch (error) {
 		console.log("error in fetching products:", error.message);
+		res.status(500).json({ success: false, message: "Server Error" });
+	}
+};
+
+export const updateProduct = async (req, res) => {
+	const { id } = req.params;
+	const product = req.body;
+
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ success: false, message: "Invalid Product Id" });
+	}
+
+	try {
+		const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
+		res.status(200).json({ success: true, data: updatedProduct });
+	} catch (error) {
 		res.status(500).json({ success: false, message: "Server Error" });
 	}
 };
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createProduct, getProducts } from "../controllers/product.controller.js";
+import { createProduct, getProducts, updateProduct } from "../controllers/product.controller.js";
 
 const router = express.Router();
 
 router.post("/", createProduct);
 router.get("/", getProducts);
+router.put("/:id", updateProduct);
 
 export default router;
