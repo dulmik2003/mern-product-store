a820c54bff130bbebd6fa0f8a1ceae55771f2fa2
Dulmika <semaldulmika@gmail.com>
2025-07-22 12:23:02 +0530

    Create the CreatePage and implement the functionality for calling the POST API for creating new products.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import { Box, useColorModeValue } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 
+import CreatePage from "./pages/CreatePage";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
       <Navbar />
+
+      <Routes>
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
     </Box>
   );
 }
