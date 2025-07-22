980aef89eaa4291eecaf6880524b9c61493c2e88
Dulmika <semaldulmika@gmail.com>
2025-07-22 10:39:10 +0530

    Create the Navbar component and add it to the main UI.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import { Button } from "@chakra-ui/react"
-
+import { Box, useColorModeValue } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <Button>Hello</Button>
-    </>
-  )
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+      <Navbar />
+    </Box>
+  );
 }
 
-export default App
+export default App;
