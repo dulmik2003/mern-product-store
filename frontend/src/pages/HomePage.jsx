ebccf104f82d73248c5b7c4d759ee98abb297638
Dulmika <semaldulmika@gmail.com>
2025-07-22 12:59:35 +0530

    Create the ProductCard component and implement the functionality for calling the GET API for retrieving all products.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,7 +5,11 @@ import { useProductStore } from "../store/product";
 import ProductCard from "../components/ProductCard";
 
 const HomePage = () => {
-	const { products } = useProductStore();
+	const { fetchProducts ,products } = useProductStore();
+
+    useEffect(() => {
+		fetchProducts();
+	}, [fetchProducts]);
 
 	return (
 		<Container maxW='container.xl' py={12}>
@@ -19,6 +23,20 @@ const HomePage = () => {
 				>
 					Current Products 🚀
 				</Text>
+
+                <SimpleGrid
+					columns={{
+						base: 1,
+						md: 2,
+						lg: 3,
+					}}
+					spacing={10}
+					w={"full"}
+				>
+					{products.map((product) => (
+						<ProductCard key={product._id} product={product} />
+					))}
+				</SimpleGrid>
 
 				{products.length === 0 && (
 					<Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
