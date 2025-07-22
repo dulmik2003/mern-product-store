afb59a46f90fc1daf96835b50b5c86a2192f9dcb
Dulmika <semaldulmika@gmail.com>
2025-07-22 13:54:54 +0530

    Add the edit button to the ProductCart and implement the functionality for calling the PUT API for updating each product.

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -24,5 +24,23 @@ export const useProductStore = create((set) => ({
 		const res = await fetch("/api/products");
 		const data = await res.json();
 		set({ products: data.data });
-	}
+	}, 
+
+	updateProduct: async (productId, updatedProduct) => {
+		const res = await fetch(`/api/products/${productId}`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(updatedProduct),
+		});
+		const data = await res.json();
+		if (!data.success) return { success: false, message: data.message };
+
+		// update the ui immediately, without needing a refresh
+		set((state) => ({
+			products: state.products.map((product) => (product._id === productId ? data.data : product)),
+		}));
+		return { success: true, message: data.message };
+	},
 }));
